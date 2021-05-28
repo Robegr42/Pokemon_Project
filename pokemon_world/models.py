9c2991440fd27afcc49e259350c24f2320ec7945
sr_Dracarys <jaime43lenin@gmail.com>
2021-05-28 03:54:00 -0400

    fix database

--- pokemon_world/models.py
+++ pokemon_world/models.py
@@ -3,40 +3,39 @@ from django.db import models
 
 class Element(models.Model):
     name = models.CharField(max_length=200,  primary_key=True)
-    
+    effective_agains = models.ManyToManyField('self' ,related_name='affected_by', null=True, blank=True)
 class Movement(models.Model):
     name = models.CharField(max_length=200,  primary_key=True)
-    element = models.ForeignKey(Element, on_delete=models.CASCADE)
-   
-class Date(models.Model):
-    id = models.CharField(max_length=200, primary_key=True)
-    
-    
+    element = models.ForeignKey('Element', on_delete=models.CASCADE, related_name='element')
+
 class Region(models.Model):
     code = models.CharField(max_length=200,  primary_key=True)
     name = models.CharField(max_length=200)
-
+    species = models.ManyToManyField('Specie', blank=True)
 class Citizen(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=200)
     sex = models.CharField(max_length=1)
     age = models.IntegerField(default=1)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
-    #l_code = models.ForeignKey(Region, on_delete=models.CASCADE)
+    born_region = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='born_region')
+    live_region = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='live_region')
        
 class City(models.Model): 
     name = models.CharField(max_length=200)
     id = models.CharField(max_length=200, primary_key=True)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    region = models.ForeignKey('Region', on_delete=models.CASCADE)
 class Gym(models.Model):
     id =  models.CharField(max_length=200, primary_key=True)
-    element = models.ForeignKey(Element, on_delete=models.CASCADE)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    element = models.ForeignKey('Element', on_delete=models.CASCADE, related_name='gym_element')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='city')
+    boss = models.ForeignKey('Trainer', on_delete=models.CASCADE, related_name='boss')
+    
 
 class Trainer(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
-    citizen = models.ForeignKey(Citizen, on_delete=models.CASCADE)
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
+    citizen = models.ForeignKey('Citizen', on_delete=models.CASCADE, related_name='citizen')
+    medals = models.ManyToManyField(Gym)
+    gym_member = models.ForeignKey('Gym', on_delete=models.SET_NULL, related_name='gym_member', null=True, blank=True)
     
 class Pokemon(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
@@ -44,78 +43,40 @@ class Pokemon(models.Model):
     weight = models.IntegerField(default=1)
     sex = models.CharField(max_length=1)
     nature = models.CharField(max_length=200)
-    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
-
-class Knows(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
-    
-class Leads(models.Model):
-    citizen = models.ForeignKey(Citizen, on_delete=models.CASCADE)
-    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
-    
-class Got_Medals(models.Model):
-    citizen = models.ForeignKey(Citizen, on_delete=models.CASCADE)
-    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
-    
+    movements = models.ManyToManyField('Movement', blank=True)
+    specie = models.ForeignKey('Specie', on_delete=models.CASCADE, related_name='specie')
+    captured_by = models.ForeignKey('Trainer', on_delete=models.SET_NULL, related_name='captured_by', null=True, blank=True)
 class Specie(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
     legendary = models.BooleanField(default=False)
-
-class Belongs(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    specie = models.ForeignKey(Specie, on_delete=models.CASCADE)
-    
-class Inhabited(models.Model): 
-    specie = models.ForeignKey(Specie, on_delete=models.CASCADE)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
-    
+    movements = models.ManyToManyField('Movement', blank=True)
 class Own(models.Model):
-    movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
+    movement = models.ForeignKey('Movement', on_delete=models.CASCADE)
 
 class Teach(models.Model):
-    movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
+    movement = models.ForeignKey('Movement', on_delete=models.CASCADE)
     
 class Settlemen(models.Model):
     id = models.CharField(max_length=200, primary_key=True, default='')
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    region = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='region')
 
 class Town(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
-    settlemen = models.ForeignKey(Settlemen, on_delete=models.CASCADE)
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    settlemen = models.ForeignKey('Settlemen', on_delete=models.CASCADE, related_name='settlemen')
 
-class Effective_Against(models.Model):
-    elemnt_1 = models.ForeignKey(Element, on_delete=models.CASCADE)
-    element_2 = models.ForeignKey(Element, on_delete=models.CASCADE, related_name='element2')
-
-class BelongsE(models.Model):
-    element = models.ForeignKey(Element, on_delete=models.CASCADE)
-    specie = models.ForeignKey(Specie, on_delete=models.CASCADE)
-    
-class KnowsM(models.Model):
-    specie = models.ForeignKey(Specie, on_delete=models.CASCADE)
-    movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
-    
 class Duel(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
-    settlemen = models.ForeignKey(Settlemen, on_delete=models.CASCADE)
-    trainer_win = models.ForeignKey(Trainer, on_delete=models.CASCADE)
-    trainer_loss = models.ForeignKey(Trainer, on_delete=models.CASCADE, related_name='trainer_loss')
-    date = models.ForeignKey(Date, on_delete=models.CASCADE)
+    settlemen = models.ForeignKey('Settlemen', on_delete=models.CASCADE, related_name='duel_settlemen')
+    trainer_win = models.ForeignKey('Trainer', on_delete=models.CASCADE, related_name='trainer_win')
+    trainer_loss = models.ForeignKey('Trainer', on_delete=models.CASCADE, related_name='trainer_loss')
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    participants = models.ManyToManyField('Pokemon')
 
 class Captured_Pokemon(models.Model):
-    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, related_name='captor')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='captured')
     level = models.IntegerField(default=1)
     a_level = models.IntegerField(default=1)
     nick = models.CharField(max_length=200)
     pockeball =  models.IntegerField(default=1)
-    
-class Plays(models.Model): 
-    duel = models.ForeignKey(Duel, on_delete=models.CASCADE)
-    t_id = models.ForeignKey(Captured_Pokemon, on_delete=models.CASCADE)
-    #pk_id = models.ForeignKey(Captured_Pokemon, on_delete=models.CASCADE)
     
